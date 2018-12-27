b76b7dcb832e5d1458a8d832ed3507eeac9d2502
majorova-vs <46103844+majorova-vs@users.noreply.github.com>
2018-12-27 23:49:00 +0300

    Add files via upload

---
+++ Enemy.cpp
@@ -0,0 +1,19 @@
+#include "Enemy.h"
+
+void Enemy::Move()
+{
+	if (name == "EasyEnemy") //Логика движения для врага с именем "EasyEnemy"
+	{
+		dx = 2;
+		body.move({ dx, 0 });
+		if (body.getPosition().x < -100) body.setPosition({ 390, body.getPosition().y });
+		if (body.getPosition().x > 390) body.setPosition({ -100, body.getPosition().y });
+	}
+	if (name == "EasyEnemy2")
+	{
+		dx = 2;
+		body.move({ -dx, 0 });
+		if (body.getPosition().x < -100) body.setPosition({ 390, body.getPosition().y });
+		if (body.getPosition().x > 390) body.setPosition({ -100, body.getPosition().y });
+	}
+}
