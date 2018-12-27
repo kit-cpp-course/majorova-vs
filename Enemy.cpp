ea2aaa3601bd8a04a35497b49913bbc4af2ea2ea
majorova-vs <46103844+majorova-vs@users.noreply.github.com>
2018-12-27 23:51:07 +0300

    Update Enemy.cpp

--- Enemy.cpp
+++ Enemy.cpp
@@ -2,18 +2,30 @@
 
 void Enemy::Move()
 {
-	if (name == "EasyEnemy") //Логика движения для врага с именем "EasyEnemy"
+	if (name == "EasyEnemy") //Р›РѕРіРёРєР° РґРІРёР¶РµРЅРёСЏ РґР»СЏ РІСЂР°РіР° СЃ РёРјРµРЅРµРј "EasyEnemy"
 	{
 		dx = 2;
 		body.move({ dx, 0 });
-		if (body.getPosition().x < -100) body.setPosition({ 390, body.getPosition().y });
-		if (body.getPosition().x > 390) body.setPosition({ -100, body.getPosition().y });
+		if (body.getPosition().x < -100)
+		{
+			body.setPosition({ 390, body.getPosition().y });
+		}
+		if (body.getPosition().x > 390)
+		{
+			body.setPosition({ -100, body.getPosition().y });
+		}
 	}
 	if (name == "EasyEnemy2")
 	{
 		dx = 2;
 		body.move({ -dx, 0 });
-		if (body.getPosition().x < -100) body.setPosition({ 390, body.getPosition().y });
-		if (body.getPosition().x > 390) body.setPosition({ -100, body.getPosition().y });
+		if (body.getPosition().x < -100)
+		{
+			body.setPosition({ 390, body.getPosition().y });
+		}
+		if (body.getPosition().x > 390) 
+		{
+			body.setPosition({ -100, body.getPosition().y });
+		}
 	}
 }
